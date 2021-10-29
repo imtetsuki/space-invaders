3a4ec62d12af9aafca6277a7b17e21d472b3c5bd
imtetsuki <hoanglanpham18@gmail.com>
2021-10-29 09:24:55 +0200

    gang

--- headers/spaceship.h
+++ headers/spaceship.h
@@ -1,7 +1,7 @@
 #ifndef SPACE_INVADERS_SPACESHIP_H
 #define SPACE_INVADERS_SPACESHIP_H
 
-typedef struct vaisseau
+/*typedef struct
 {
     char Direction ; /*N => Nord, S => Sud, E => EST, O => OUEST*/
     int PosX; /*Position courante coin gauche X du vaisseau*/
@@ -14,9 +14,29 @@ typedef struct vaisseau
     char Couleur[30]; /*Couleur du vaisseau*/
     int Etat; /*État du vaisseau 1=> actif, 2=> en destruction, 3 => inactif*/
     int Mise_a_jour; /*utile pour la suppression du vaisseau en tenant compte d’un certain délai*/
-    struct vaisseau * NXT; /*Pointeur vers un prochain vaisseau, servira pour la liste chaînée*/
-/*Vous pouvez rajouter d’autres variables si nécessaire */
+    struct spaceship * NXT; /*Pointeur vers un prochain vaisseau, servira pour la liste chaînée*/
+/*Vous pouvez rajouter d’autres variables si nécessaire
+}spaceship;*/
+
+typedef struct {
+    int posX; //Position sur l'axe x du vaisseau allié
+    int posY; //Position sur l'axe y du vaisseau allié
+    int vie; //Nombre de point de vie restante
+    char** Carrosserie; //Affichage du vaisseau
+    char couleur[30]; //Couleur du vaisseau allié
+    //int Etat;
+    int Maj; // Va etre utile pour la suppression du vaisseau avec un delai
 }spaceship;
 
+typedef struct {
+    int posX; //Position sur l'axe x du vaisseau ennemie
+    int posY; //Position sur l'axe y du vaisseau ennemie
+    int vie; //Nombre de point de vie restante ( le blindage )
+    char** Carrosserie; //Affichage du vaisseau
+    char couleur[30]; //Couleau du vaiseau ennemie
+    //int etat;
+    int maj; // Va etre utile pour la suppression du vaisseau avec un delai
+    struct alien * nxt; // Un pointeur pour une liste chainée
+}alien ;
 
 #endif //SPACE_INVADERS_SPACESHIP_H
